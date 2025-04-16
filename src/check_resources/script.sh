b59976db52e6ec57ffb1ae3553f86a5e8f6cfbfa
jakubmajercik <jakub.majercik@gmail.com>
2025-04-16 15:24:18 +0200

    update params

--- src/check_resources/script.sh
+++ src/check_resources/script.sh
@@ -1,6 +1,7 @@
 usage_percentage=$(df -h "$(pwd)" | awk 'NR==2 {print $5}' | tr -d '%')
 df -h "$(pwd)"
 
-if [ "$usage_percentage" -gt "$par_threshold" ]; then
-    echo "WARNING: Disk usage is at ${usage_percentage}%"
+max_allowed_usage=$((100-par_req_disk_space))
+if [ "$usage_percentage" -gt "$max_allowed_usage" ]; then
+    echo "WARNING: Disk usage is at ${usage_percentage}%, exceeding maximum allowed usage of ${max_allowed_usage}%"
 fi
